36960c475cad270c82443628d4e9529cd4bc4c25
Oliver Kocsis <oliverkocsis@sciapps.io>
2018-04-30 13:13:46 +0100

    Initial Commit

---
+++ OKONotificationCentre/OKONotificationCentre.h
@@ -0,0 +1,19 @@
+//
+//  OKONotificationCentre.h
+//  OKONotificationCentre
+//
+//  Created by Kocsis Olivér on 2018. 04. 30..
+//  Copyright © 2018. okocsis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OKONotificationCentre.
+FOUNDATION_EXPORT double OKONotificationCentreVersionNumber;
+
+//! Project version string for OKONotificationCentre.
+FOUNDATION_EXPORT const unsigned char OKONotificationCentreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OKONotificationCentre/PublicHeader.h>
+
+
